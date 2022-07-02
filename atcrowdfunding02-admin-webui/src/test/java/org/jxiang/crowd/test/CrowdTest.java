efdab55d8234dd7c1b1574a2b5bbedf56aabe560
jxiang168 <jxiang168@163.com>
2022-07-02 16:40:58 +0800

    commit

---
+++ atcrowdfunding02-admin-webui/src/test/java/org/jxiang/crowd/test/CrowdTest.java
@@ -0,0 +1,30 @@
+package org.jxiang.crowd.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+/**
+ * TODO
+ *
+ * @author Jxiang
+ * @version 1.0
+ * @create 2022-07-02 16:23
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring-persist-mybatis.xml"})
+public class CrowdTest {
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    public void testConnection() throws Exception {
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+    }
+}
